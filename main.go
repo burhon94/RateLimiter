946b94c1117e1cc1a1f34caf778aa9fcb0ed3e4a
Burhonjon Tohirov <burhon-tj@mail.ru>
2020-07-04 11:19:15 +0500

    feat rate Limiter for Request to API

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"golang.org/x/time/rate"
+	"log"
+	"net/http"
+	"rateLimit/limiter/limit"
+	"strings"
+	"time"
+)
+
+// set limmiter allow 1 request every 20 second on from IP
+var limiter = limit.NewIPRateLimiter(rate.Every(20*time.Second), 1)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", okHandler)
+
+	addr := "192.168.8.101:9999"
+	log.Printf("listining: %s\n", addr)
+	if err := http.ListenAndServe(addr, limitMiddleware(mux)); err != nil {
+		log.Fatalf("can' t start: %s", err.Error())
+	}
+}
+
+func limitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter := limiter.GetLimiter(getIPFromRemoteAddr(r.RemoteAddr))
+
+		if !limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	ip := getIPFromRemoteAddr(r.RemoteAddr)
+	log.Printf("ip: %s\n", ip)
+
+	w.Write([]byte("status OK"))
+}
+
+func getIPFromRemoteAddr(remoteAddr string) (ip string) {
+	trim := strings.Split(remoteAddr, ":")
+	ip = trim[0]
+	return
+}
